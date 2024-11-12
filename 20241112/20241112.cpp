dfcde78520302d5880d6268b72f077df9d10f655
卓韋辰(ZhuoWeiChen) <4b1g0016@stust.edu.tw>
2024-11-12 11:11:38 +0800

    switch 迴圈

--- 20241112/20241112.cpp
+++ 20241112/20241112.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	int month = 1;  //宣告
-	string result =  "";
+	string result = "";
 	cout << "請輸入月份(1-12): ";
 	cin >> month;
 
@@ -13,10 +13,28 @@ int main()
 	}
 	else
 	{
-		if (month >= 3 && month <= 5) result = "春季";
-		else if (month >= 6 && month <= 8) result = "夏季";
-		else if (month >= 9 && month <= 11) result = "秋季";
-		else result = "冬季";
+		switch (month) {
+		case 3:
+		case 4:
+		case 5:
+			result = "春季";
+			break;
+		case 6:
+		case 7:
+		case 8:
+			result = "夏季";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			result = "秋季";
+			break;
+		case 12:
+		case 1:
+		case 2:
+			result = "冬季";
+			break;
+		}
 		cout << month << "月是" << result << endl;
 	}
 }
