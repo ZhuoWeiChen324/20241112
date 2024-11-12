d6ca6a7efca211a0c5c5f8d171a80080142eb7f9
卓韋辰(ZhuoWeiChen) <4b1g0016@stust.edu.tw>
2024-11-12 11:09:18 +0800

    季節月份程式

--- 20241112/20241112.cpp
+++ 20241112/20241112.cpp
@@ -13,28 +13,10 @@ int main()
 	}
 	else
 	{
-		switch (month) {
-		case 3:
-		case 4:
-		case 5:
-			result = "春季";
-			break;
-		case 6:
-		case 7:
-		case 8:
-			result = "夏季";
-			break;
-		case 9:
-		case 10:
-		case 11:
-			result = "秋季";
-			break;
-		case 12:
-		case 1:
-		case 2:
-			result = "冬季";
-			break;
-		}
+		if (month >= 3 && month <= 5) result = "春季";
+		else if (month >= 6 && month <= 8) result = "夏季";
+		else if (month >= 9 && month <= 11) result = "秋季";
+		else result = "冬季";
 		cout << month << "月是" << result << endl;
 	}
 }
